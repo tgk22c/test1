292abc1dc43ebfac00ff420c1736b9c967cc09f1
tgk22c <tgk22c@naver.com>
2023-10-08 19:57:45 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,25 +5,26 @@ from tensorflow.keras.datasets import mnist
 
 app = Flask(__name__)
 
-# 모델 로드
+# Load pre-trained model when the app starts.
 model = load_model('model.h5')
+
+# Load the MNIST dataset.
+(_, _), (x_test, y_test) = mnist.load_data()
+x_test = x_test.reshape(10000, 784).astype('float32') / 255.0
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        # MNIST 데이터셋에서 랜덤한 이미지 선택 
-        (x_train, y_train), (x_test, y_test) = mnist.load_data()
-        rand_index = np.random.randint(0, x_test.shape[0])
-        image = x_test[rand_index]
-        
-        # 이미지 전처리 및 예측 수행 
-        image = image.reshape(1, 28, 28)
-        prediction = model.predict(image).argmax()
+        # Select a random test image and its label.
+        i = np.random.randint(0, len(x_test))
+        image, true_label = x_test[i], y_test[i]
 
-        return render_template('index.html', prediction=prediction,
-                               actual=y_test[rand_index],
-                               img_data=image.reshape(28, 28))
-    return render_template('index.html')
+        # Predict the digit in the image.
+        pred_label = model.predict_classes(image.reshape(1, -1))
 
-if __name__ == "__main__":
+        return render_template('index.html', pred_label=pred_label[0], true_label=true_label)
+    else:
+        return render_template('index.html')
+
+if __name__ == '__main__':
     app.run(debug=True)
